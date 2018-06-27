93a39a5c365ca9cd965a0d455712e9b58769a61f
Isaac Galvan Lanado <igalvan@MacBook-Pro-de-Isaac.local>
2018-06-27 17:29:12 -0500

    added macos prereqs and some bug fixes

---
+++ prepare-macos-prereqs.sh
@@ -0,0 +1,8 @@
+sudo xcode-select --install
+sudo easy_install pip
+sudo pip install ansible --quiet
+sudo pip install ansible --upgrade
+sudo pip install apache-libcloud
+sudo pip install pycrypto
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install http://git.io/sshpass.rb
